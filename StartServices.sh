57919ee8ce06fd5dbd29514da6a75bb5e738e0f9
AlfredoCastro08 <45361748+AlfredoCastro08@users.noreply.github.com>
2019-07-10 10:18:29 -0500

    echo's

--- StartServices.sh
+++ StartServices.sh
@@ -2,20 +2,26 @@
 
 #INICAR IPTABLES
 systemctl start iptables
+echo "INICIANDO IPTABLES"
 
 #INICAR DCHP
 systemctl start dhcpd 
+echo "INICIANDO DCHP"
 
 #INICAR SSH
 systemctl start sshd 
+echo "INICIANDO SSH"
 
 #INICAR APACHE 
 systemctl start httpd 
+echo "INICIANDO APACHE"
 
 #INICAR FTP
 systemctl start vsftpd
+echo "INICIANDO FTP"
 
 #INICAR SAMBA
 systemctl start smb
 
 systemctl start nmb
+echo "INICIANDO SAMBA "
